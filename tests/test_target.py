30d78527341f869e85453968f78dd119cae42a20
Nicholas Bishop <nicholasbishop@gmail.com>
2018-01-18 13:01:59 -0500

    Reformat

--- tests/test_target.py
+++ tests/test_target.py
@@ -2,10 +2,11 @@ import unittest
 
 from ssh_target import SshTarget
 
+
 class TestTarget(unittest.TestCase):
     def test_str_default_port(self):
         self.assertEqual(str(SshTarget('host', 'user')), 'user@host')
 
     def test_str_with_port(self):
-        self.assertEqual(str(SshTarget('host', 'user', 2222)),
-                         'user@host:2222')
+        self.assertEqual(
+            str(SshTarget('host', 'user', 2222)), 'user@host:2222')
