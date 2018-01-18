2c4e00fd04a69230b4b00776261c0c79249a6dd9
Nicholas Bishop <nicholasbishop@gmail.com>
2018-01-18 13:01:24 -0500

    Implement string for non-default port

--- ssh_target/__init__.py
+++ ssh_target/__init__.py
@@ -2,9 +2,14 @@ import attr
 
 @attr.s
 class SshTarget:
+    DefaultPort = 22
+
     host = attr.ib()
     user = attr.ib()
-    port = attr.ib(default=22)
+    port = attr.ib(default=DefaultPort)
 
     def __str__(self):
-        return '{}@{}'.format(self.user, self.host)
+        host_and_port = self.host
+        if self.port != self.DefaultPort:
+            host_and_port = '{}:{}'.format(self.host, self.port)
+        return '{}@{}'.format(self.user, host_and_port)
--- tests/test_target.py
+++ tests/test_target.py
@@ -5,3 +5,7 @@ from ssh_target import SshTarget
 class TestTarget(unittest.TestCase):
     def test_str_default_port(self):
         self.assertEqual(str(SshTarget('host', 'user')), 'user@host')
+
+    def test_str_with_port(self):
+        self.assertEqual(str(SshTarget('host', 'user', 2222)),
+                         'user@host:2222')
